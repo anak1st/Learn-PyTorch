0a0d96284662c5ca9f1b7dab781dd46627aa8e5c
anak1st <xiafan8050@outlook.com>
2022-08-05 10:39:53 +0800

    move train

--- mnist/train.py
+++ mnist/train.py
@@ -47,11 +47,6 @@ def print_dataset():
     plt.show()
 
 
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
