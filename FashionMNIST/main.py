9db9d5ba999f8d86f406b217b327f82e6a574727
Anak1st <xiafan8050@outlook.com>
2022-10-17 18:27:14 +0800

    delete useless files

--- FashionMNIST/main.py
+++ FashionMNIST/main.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -83,31 +84,32 @@ def test(dataloader, model, loss_fn):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error:\n  Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
 if __name__ == "__main__":
-    for x, y in test_dataloader:
-        print(f"Shape of x [N, C, H, W]: {x.shape}")
-        print(f"Shape of y: {y.shape} {y.dtype}\n")
-        break
+    # for x, y in test_dataloader:
+    #     print(f"Shape of x [N, C, H, W]: {x.shape}")
+    #     print(f"Shape of y: {y.shape} {y.dtype}\n")
+    #     break
 
-    print(f"Using {device} device\n")
+    print(f"Using [Device: {device}]")
     
     model = NeuralNetwork().to(device)
-    model.load_state_dict(torch.load(model_path))
-    print(f"{model}\n")
+    if os.path.isfile(model_path):
+        model.load_state_dict(torch.load(model_path))
+        print(f"Load PyTorch Model from {model_path}\n")
+    # print(f"{model}\n")
 
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-    epochs = 100
+    epochs = 10
     run_train = True
     if run_train:
         for t in range(epochs):
             print(f"Epoch {t+1}\n-------------------------------")
             train(train_dataloader, model, loss_fn, optimizer)
             test(test_dataloader, model, loss_fn)
-        print("Done!")
-
+        
         torch.save(model.state_dict(), model_path)
         print(f"Saved PyTorch Model State to {model_path}")
